99d7924dfd6b68a42869cab3ead0a50d1ae72265
tiny <tiny@nzxt.>
2024-03-02 17:49:40 -0800

    Remove huggingface token that I stupidly committed. It's already invalidated so don't even bother.

--- vllmrun.sh
+++ vllmrun.sh
@@ -3,7 +3,7 @@ set -e
 podman run --rm --gpus all \
     --device nvidia.com/gpu=all \
     -v /home/andy/mnt/drive/.cache/huggingface:/root/.cache/huggingface \
-    --env "HUGGING_FACE_HUB_TOKEN=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" \
+    --env "HUGGING_FACE_HUB_TOKEN=<replace>" \
     -p 8000:8000 \
     --ipc=host \
     vllm/vllm-openai:latest \
