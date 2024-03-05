726ab17ebd5fab88b418a2050f514327d1264aff
Andy Salerno <andysalerno@gmail.com>
2024-03-04 17:03:36 -0800

    update vllmrun.sh and tgirun.sh

--- vllmrun.sh
+++ vllmrun.sh
@@ -2,12 +2,8 @@
 set -e
 podman run --rm --gpus all \
     --device nvidia.com/gpu=all \
-    -v /home/andy/mnt/drive/.cache/huggingface:/root/.cache/huggingface \
-    --env "HUGGING_FACE_HUB_TOKEN=<replace>" \
+    -v /home/tiny/.cache/huggingface:/root/.cache/huggingface \
     -p 8000:8000 \
-    --ipc=host \
     vllm/vllm-openai:latest \
     --model $1 \
-    --revision $2 \
-    --dtype float16 \
     --max-model-len 8192
