dd279f8932d74437995a0c331c213db5bd36b4ea
andy <andysalerno@gmail.com>
2024-04-21 22:13:55 -0700

    flush printlines and remove unused dependency from embedding-server

--- embedding-server/embedding_server.py
+++ embedding-server/embedding_server.py
@@ -3,29 +3,32 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from sentence_transformers import SentenceTransformer
 
 
+def log(message: str):
+    print(message, flush=True)
+
+
 # EMBEDDING_MODEL_NAME = "all-mpnet-base-v2"
 # EMBEDDING_MODEL_NAME = "multi-qa-MiniLM-L6-cos-v1"
 # EMBEDDING_MODEL_NAME = "all-MiniLM-L6-v2"
 EMBEDDING_MODEL_NAME = "intfloat/e5-small-v2"
 
-print("initializing embedding transformer...")
+log("initializing embedding transformer...")
 embedding_model = SentenceTransformer(EMBEDDING_MODEL_NAME)
-print("done.")
+log("done.")
 
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_POST(self):
-        print(f"POST {self.path}", flush=True)
+        log(f"POST {self.path}")
 
         if self.path == "/embeddings":
             self.handle_embeddings()
 
     def handle_embeddings(self):
-        print("embeddings requested")
+        log("embeddings requested")
 
         content_length = int(self.headers["Content-Length"])
         body = json.loads(self.rfile.read(content_length).decode("utf-8"))
-        # body = json.loads(self.rfile.read())
 
         text = body["input"] if "input" in body else body["text"]
 
@@ -55,17 +58,18 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header("Content-Type", "application/json")
         self.end_headers()
         self.wfile.write(response.encode("utf-8"))
+        log("embeddings request completed")
 
 
 def run(server_class=HTTPServer, handler_class=MyHandler):
-    print("warming up embedding model...", flush=True)
+    log("warming up embedding model...")
     embedding_model.encode("blank text to trigger deployment to GPU")
-    print("done.", flush=True)
+    log("done.")
 
     port = 8000
     server_address = ("0.0.0.0", port)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting httpd. Listening on port {port}...\n", flush=True)
+    log(f"Starting httpd. Listening on port {port}...\n")
     httpd.serve_forever()
 
 
