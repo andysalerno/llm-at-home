2e43f6061c28ebcc60baa5028af550c93f0deac1
Andy Salerno <andysalerno@gmail.com>
2024-04-23 01:01:25 +0000

    Begin adding openai api example

---
+++ agentflow/Examples/Examples/OpenAIServerExample.cs
@@ -0,0 +1,56 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using AgentFlow;
+using Microsoft.Extensions.Logging;
+
+internal class OpenAIServerExample
+{
+    public static async Task ServeAsync(string[] args)
+    {
+        var logger = Logging.Factory.CreateLogger<OpenAIServerExample>();
+
+        var listener = new HttpListener();
+        listener.Prefixes.Add("http://0.0.0.0:8003/");
+        listener.Start();
+
+        await HandleIncomingConnections(listener, logger);
+
+        listener.Close();
+    }
+
+    private static async Task HandleIncomingConnections(HttpListener listener, ILogger<OpenAIServerExample> logger)
+    {
+        while (true)
+        {
+            logger.LogInformation("Waiting for request...");
+            HttpListenerContext ctx = await listener.GetContextAsync(); // Wait for a request
+            HttpListenerRequest request = ctx.Request;
+            HttpListenerResponse response = ctx.Response;
+            logger.LogInformation("Incoming request received...");
+
+            ChatCompletionRequest chatRequest;
+            using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                string content = await reader.ReadToEndAsync();
+                chatRequest = JsonSerializer.Deserialize<ChatCompletionRequest>(content)
+                    ?? throw new InvalidOperationException($"Could not parse request type as a ChatCompletionRequest: {content}");
+            }
+
+            // Write the response info
+            string responseString = @"{ ""text"": ""example""  }";
+            byte[] data = Encoding.UTF8.GetBytes(responseString);
+            response.ContentType = "application/json";
+            response.ContentEncoding = Encoding.UTF8;
+            response.ContentLength64 = data.LongLength;
+
+            // Write out to the response stream (asynchronously), then close it
+            logger.LogInformation("Responding...");
+            await response.OutputStream.WriteAsync(data, 0, data.Length);
+            response.Close();
+            logger.LogInformation("Request complete.");
+        }
+    }
+
+    private record ChatCompletionRequest(string Dummy);
+}
