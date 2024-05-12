566cf091381e866521b57338c26d817636fa1b1b
andy <andysalerno@gmail.com>
2024-05-11 19:20:40 -0700

    update model name

--- scripts/vllmrun.sh
+++ scripts/vllmrun.sh
@@ -7,7 +7,7 @@ podman run --rm --gpus all \
     -p 8000:8000 \
     vllm/vllm-openai:latest \
     --model $1 \
-    --served-model-name model \
+    --served-model-name mixtral-8x7b-32768 \
     --max-model-len 8192 \
     --kv-cache-dtype fp8 \
     -tp 1 -pp 1 --trust-remote-code
