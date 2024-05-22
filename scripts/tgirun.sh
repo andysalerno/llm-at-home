4f5d20bd110bbf59fcc8d1ef7f90eb6a438a84a8
andy <andysalerno@gmail.com>
2024-05-21 23:32:33 -0700

    update tgirun

--- scripts/tgirun.sh
+++ scripts/tgirun.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/bash
 set -e
-QUANTIZATION=eetq
-MAX_TOKENS=8192
-MAX_INPUT=8191
+# QUANTIZATION=eetq
+QUANTIZATION=bitsandbytes
+# MAX_TOKENS=8192
+# MAX_INPUT=8191
 
-MODEL_ID=openchat/openchat-3.5-0106
-MODEL_REVISION=9619fb7d2a8e25fa6b0633c0f57f7f4aa79b45c4
+MODEL_ID=microsoft/Phi-3-small-8k-instruct
+# MODEL_REVISION=9619fb7d2a8e25fa6b0633c0f57f7f4aa79b45c4
 
 podman run --rm --gpus all \
     --device nvidia.com/gpu=all \
-    -v /home/tiny/.cache/huggingface:/data \
+    -v /home/tiny/mnt/drive/.cache/huggingface:/data \
     -p 8080:8000 \
-    ghcr.io/huggingface/text-generation-inference:sha-39af000 \
+    ghcr.io/huggingface/text-generation-inference:latest \
     --model-id ${MODEL_ID} \
+    --port 8000 \
     --quantize ${QUANTIZATION} \
-    --max-best-of 1 \
-    --max-concurrent-requests 2 \
-    --validation-workers 1 \
-    --max-batch-prefill-tokens ${MAX_TOKENS} \
-    --max-total-tokens ${MAX_TOKENS} \
-    --max-input-length ${MAX_INPUT} \
-    --max-batch-total-tokens ${MAX_TOKENS} \
-    --max-top-n-tokens 1 \
-    --revision ${MODEL_REVISION}
+    # --max-best-of 1 \
+    # --max-concurrent-requests 2 \
+    # --validation-workers 1 \
+    # --max-batch-prefill-tokens ${MAX_TOKENS} \
+    # --max-total-tokens ${MAX_TOKENS} \
+    # --max-input-length ${MAX_INPUT} \
+    # --max-batch-total-tokens ${MAX_TOKENS} \
+    # --max-top-n-tokens 1 \
+    # --revision ${MODEL_REVISION}
