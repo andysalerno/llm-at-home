219ba311dc582b0028ba589e991c97bdcb22c94c
Andy Salerno <andysalerno@gmail.com>
2024-06-26 03:30:02 +0000

    Add PromptTests, to be filled in

---
+++ agentflow/ExecutionFlow.Tests/PromptTests.cs
@@ -0,0 +1,18 @@
+using AgentFlow;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace ExecutionFlow.Tests;
+
+public class PromptTests
+{
+    public PromptTests()
+    {
+        Logging.TryRegisterLoggerFactory(NullLoggerFactory.Instance);
+    }
+
+    [Fact]
+    public void Sample()
+    {
+        Assert.True(true);
+    }
+}
