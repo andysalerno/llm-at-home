bcbc8fb2dcc80468208ff974cb0b7e432c4414bc
Andy Salerno <andysalerno@gmail.com>
2024-06-30 07:18:11 +0000

    frontmatter parsing enabled

--- agentflow/AgentFlow.Tests/PromptTests.cs
+++ agentflow/AgentFlow.Tests/PromptTests.cs
@@ -36,4 +36,21 @@ this is some prompt
 
         Assert.Equal(expected: "this is some prompt", actual: prompt.Render().Text);
     }
+
+    [Fact]
+    public void PromptFrontMatter_WithFrontMatter_CanBeParsed()
+    {
+        const string PromptText = """
+---
+name: some prompt
+---
+this is some prompt
+""";
+
+        var parser = new PromptParser();
+
+        var prompt = parser.Parse(PromptText);
+
+        Assert.Equal(expected: "some prompt", actual: prompt.PromptFrontMatter.Name);
+    }
 }
--- agentflow/AgentFlow/Prompts/PromptParser.cs
+++ agentflow/AgentFlow/Prompts/PromptParser.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
 namespace AgentFlow.Prompts;
 
 public interface IPromptParser
@@ -7,6 +11,11 @@ public interface IPromptParser
 
 public sealed class PromptParser : IPromptParser
 {
+    private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true,
+    };
+
     public Prompt Parse(string input)
     {
         input = input.Replace("\r\n", "\n", StringComparison.Ordinal);
@@ -18,6 +27,27 @@ public sealed class PromptParser : IPromptParser
             3,
             StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
-        return new Prompt(splits[1], new Prompt.FrontMatter(splits[0]));
+        return new Prompt(splits[1], ParseFrontMatter(splits[0]));
+    }
+
+    private static Prompt.FrontMatter ParseFrontMatter(string yaml)
+    {
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+
+        // string --> yaml
+        var yamlObj = deserializer.Deserialize(yaml);
+
+        // yaml --> json
+        string json = new SerializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .JsonCompatible()
+            .Build()
+            .Serialize(yamlObj);
+
+        // json --> FrontMatter
+        return JsonSerializer.Deserialize<Prompt.FrontMatter>(json, JsonOptions)
+            ?? throw new InvalidOperationException("Could not deserialize frontmatter");
     }
 }
