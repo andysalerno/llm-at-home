fbf6628120f4356882c0fc3cd2e8f7b2c5291ebe
Andy Salerno <andysalerno@gmail.com>
2024-07-06 16:59:41 -0700

    remove scripts dir

--- scripts/llamacpprun.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-set -e
-
-podman run --rm --gpus all \
-    --device nvidia.com/gpu=all \
-    -v /home/tiny/mnt/drive/gguf:/models \
-    -p 8003:8080 \
-    localhost/llama.cpp:server-cuda \
-    -m /models/Phi-3-mini-128k-instruct.Q8_0.gguf --port 8080 --host 0.0.0.0 -ngl 100 -fa
