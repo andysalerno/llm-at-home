a1b6f18915e6cba9a448a7aff8c0f7edda57d3b0
andy <andysalerno@gmail.com>
2024-09-10 00:11:02 -0700

    improve sidebar

--- agentflow-ui/src/App.jsx
+++ agentflow-ui/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
                 <div className="flex-1 flex flex-col overflow-hidden">
                     <header className="bg-white shadow-sm z-10">
                         <div className="max-w-7xl mx-auto py-4 px-4">
-                            <h1 className="text-lg font-semibold text-gray-900">My Web App</h1>
+                            <h1 className="text-lg font-semibold text-gray-900">Agentflow</h1>
                         </div>
                     </header>
                     <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
--- agentflow-ui/src/components/Sidebar.jsx
+++ agentflow-ui/src/components/Sidebar.jsx
@@ -14,7 +14,15 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                     <nav class="-mx-3 space-y-6 ">
                         <div class="space-y-3 ">
                             <label class="px-3 text-xs text-gray-500 uppercase dark:text-gray-400">AgentFlow UI</label>
+                            <Link to="/chat">
+                                <div class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                                    </svg>
 
+                                    <span class="mx-2 text-sm font-medium">Chat</span>
+                                </div>
+                            </Link>
                             <Link to="/debug">
                                 <div class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
@@ -25,15 +33,12 @@ const Sidebar = ({ isOpen, setIsOpen }) => {
                                     <span class="mx-2 text-sm font-medium">Debug</span>
                                 </div>
                             </Link>
-
-                            <Link to="/chat">
+                            <Link to="/debug">
                                 <div class="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
                                     </svg>
-
-                                    <span class="mx-2 text-sm font-medium">Chat</span>
+                                    <span class="mx-2 text-sm font-medium">Configure</span>
                                 </div>
                             </Link>
                         </div>
