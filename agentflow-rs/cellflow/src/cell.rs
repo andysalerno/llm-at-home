6513bd84c848c0dea85b91cd072705bcdf69a56e
andy <andysalerno@gmail.com>
2024-10-31 20:42:00 -0700

    continue implementing the cells

--- agentflow-rs/cellflow/src/cell.rs
+++ agentflow-rs/cellflow/src/cell.rs
@@ -1,17 +1,64 @@
+use serde::{Deserialize, Serialize};
+
+/// A serializable and deserializable representation of some operation.
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Cell {
     If(IfCell),
     While(WhileCell),
     Sequence(SequenceCell),
-    Custom,
+    Custom(Id),
 }
 
-pub struct Condition;
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq, PartialOrd, Ord)]
+pub struct Id(String);
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Condition {
+    id: Id,
+}
+
+impl Condition {
+    pub fn id(&self) -> &Id {
+        &self.id
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct IfCell {
+    condition: Condition,
     on_true: Box<Cell>,
     on_false: Box<Cell>,
 }
 
-pub struct WhileCell;
+impl IfCell {
+    pub fn condition(&self) -> &Condition {
+        &self.condition
+    }
 
+    pub fn on_true(&self) -> &Cell {
+        &self.on_true
+    }
+
+    pub fn on_false(&self) -> &Cell {
+        &self.on_false
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct WhileCell {
+    condition: Condition,
+    body: Box<Cell>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SequenceCell(Vec<Cell>);
+
+#[cfg(test)]
+mod tests {
+    use super::{Cell, SequenceCell};
+
+    #[test]
+    fn test_simple_sequence() {
+        let _cell = Cell::Sequence(SequenceCell(Vec::new()));
+    }
+}
--- agentflow-rs/cellflow/src/visitor.rs
+++ agentflow-rs/cellflow/src/visitor.rs
@@ -1,26 +1,70 @@
-use crate::cell::Cell;
+use crate::{cell::Cell, Id};
 
+/// A trait representing a handler for a type of cell.
+/// It maps to the cell type by id.
 pub trait CellHandler<T> {
-    fn id(&self) -> String;
+    fn id(&self) -> Id;
     fn handle(&self, item: &T) -> T;
 }
 
+pub trait ConditionHandler<T> {
+    fn id(&self) -> Id;
+    fn handle(&self, item: &T) -> bool;
+}
+
+enum Handler<T> {
+    Cell(Box<dyn CellHandler<T>>),
+    Condition(Box<dyn ConditionHandler<T>>),
+}
+
+/// A visitor or interpreter that can visit a cell
+/// and perform the given handler for it.
 pub struct CellVisitor<T> {
-    handlers: Vec<Box<dyn CellHandler<T>>>,
+    handlers: Vec<Handler<T>>,
 }
 
 impl<T> CellVisitor<T> {
-    pub fn visit(cell: &Cell, input: &T) {
+    pub fn visit(&self, cell: &Cell, input: &T) {
         match cell {
-            Cell::If(if_cell) => todo!(),
+            Cell::If(if_cell) => {
+                let condition_handler = self.select_condition(if_cell.condition().id());
+            }
             Cell::While(while_cell) => todo!(),
             Cell::Sequence(sequence_cell) => todo!(),
-            Cell::Custom => todo!(),
+            Cell::Custom(id) => todo!(),
         }
     }
 
-    fn select_handler(&self, input: &T) -> &dyn CellHandler<T> {
-        self.handlers.first().unwrap().as_ref()
+    pub fn run(&self, program: &Cell, input: &T) {}
+
+    fn select_handler(&self, id: &Id) -> &dyn CellHandler<T> {
+        let found = self
+            .handlers
+            .iter()
+            .filter_map(|h| match h {
+                Handler::Cell(handler) => Some(handler),
+                Handler::Condition(_) => None,
+            })
+            .filter(|h| h.id() == *id)
+            .next()
+            .expect("expected a condition to be registered");
+
+        found.as_ref()
+    }
+
+    fn select_condition(&self, id: &Id) -> &dyn ConditionHandler<T> {
+        let found = self
+            .handlers
+            .iter()
+            .filter_map(|h| match h {
+                Handler::Cell(_) => None,
+                Handler::Condition(handler) => Some(handler),
+            })
+            .filter(|h| h.id() == *id)
+            .next()
+            .expect("expected a condition to be registered");
+
+        found.as_ref()
     }
 }
 
@@ -29,8 +73,8 @@ mod tests {
     use super::CellHandler;
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected = "checking object safety")]
     fn verify_object_safety() {
-        let _: Box<dyn CellHandler<usize>> = todo!();
+        let _unused: Box<dyn CellHandler<usize>> = todo!("checking object safety");
     }
 }
