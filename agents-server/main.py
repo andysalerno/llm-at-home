029963cc38c531995e2779444020c96362790908
andy <andysalerno@gmail.com>
2025-03-23 13:41:12 -0700

    add env vars

--- agents-server/main.py
+++ agents-server/main.py
@@ -1,6 +1,52 @@
-def main():
-    print("Hello from agents-server!")
+import os
+from smolagents import (
+    OpenAIServerModel,
+    VisitWebpageTool,
+    DuckDuckGoSearchTool,
+    CodeAgent,
+)
 
+from phoenix.otel import register
+from openinference.instrumentation.smolagents import SmolagentsInstrumentor
 
-if __name__ == "__main__":
-    main()
+register()
+SmolagentsInstrumentor().instrument()
+
+OPENROUTER_KEY = os.environ.get("LLM_API_KEY")
+
+if OPENROUTER_KEY is None:
+    raise ValueError("Please set the environment variable 'LLM_API_KEY'")
+
+model = OpenAIServerModel(
+    model_id="google/gemini-2.0-flash-lite-preview-02-05:free",
+    api_key=OPENROUTER_KEY,
+    api_base="https://openrouter.ai/api/v1",
+)
+
+search_tool = DuckDuckGoSearchTool()
+search_tool.description = "Performs a google search based on your query then returns a summary of the top results with their respective urls."
+
+# source_finder = ToolCallingAgent(
+#     tools=[search_tool],
+#     model=model,
+#     name="source_finder_agent",
+#     description="Runs web searches for you to find suitable websites to visit. Give it your query as an argument called 'task'. Since you don't have access to real-time information, use this agent to find real-time information, news, weather, current events, etc.",
+# )
+
+research_agent = CodeAgent(
+    tools=[search_tool, VisitWebpageTool()],
+    managed_agents=[],
+    model=model,
+    name="research_agent",
+    description='Performs research and responds with results. Can search the web, find real-time information, and summarize results. Provide your query as an argument called "task".',
+)
+
+research_agent.prompt_templates["system_prompt"] += (
+    "\nAdditional info:"
+    + "\n- If you visit multiple websites, remember that any may fail and throw an error, so try/catch for each."
+    + "\n- After invoking visit_webpage, print its output to capture the result, otherwise it will be lost."
+)
+
+main_agent = CodeAgent(tools=[], managed_agents=[research_agent], model=model)
+
+main_agent.run(task="What movies are playing in Kirkland, WA this weekend?")
