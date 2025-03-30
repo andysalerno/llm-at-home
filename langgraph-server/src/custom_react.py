a0cfdecde94cd29817bdcc1de1f970582920ba3d
Andy Salerno <andysalerno@gmail.com>
2025-03-29 23:29:30 -0700

    tallest building faq example is working

---
+++ langgraph-server/src/custom_react.py
@@ -0,0 +1,59 @@
+from typing import Callable
+from langgraph.prebuilt.tool_node import ToolNode
+from langchain_core.language_models import BaseChatModel
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.graph.state import CompiledStateGraph
+
+
+class ChatState(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+def create_simple_chat_agent(model: BaseChatModel) -> CompiledStateGraph:
+    def _simple_chat(state: ChatState) -> ChatState:
+        return {"messages": [model.invoke(state["messages"])]}
+
+    graph_builder = StateGraph(ChatState)
+    graph_builder.add_edge(START, "simple_chat")
+    graph_builder.add_node("simple_chat", _simple_chat)
+    graph_builder.add_edge("simple_chat", END)
+
+    graph = graph_builder.compile()
+
+    return graph
+
+
+def create_custom_react_agent(model: BaseChatModel, tools: list[Callable]):
+    model_with_tools = model.bind_tools(tools)
+
+    def _simple_chat(state: ChatState) -> ChatState:
+        return {"messages": [model_with_tools.invoke(state["messages"])]}
+
+    graph_builder = StateGraph(ChatState)
+
+    graph_builder = StateGraph(ChatState)
+    graph_builder.add_edge(START, "simple_chat")
+    graph_builder.add_node("simple_chat", _simple_chat)
+
+    graph_builder.add_node("handle_tools", ToolNode(tools))
+    graph_builder.add_conditional_edges("simple_chat", maybe_route_tool)
+    graph_builder.add_edge("handle_tools", "simple_chat")
+
+    # tool_node = ToolNode(tools)
+    # tool_classes = list(tool_node.tools_by_name.values())
+
+    return graph_builder.compile()
+
+
+def maybe_route_tool(state: ChatState):
+    state["messages"]
+    if messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "handle_tools"
+    return END
--- langgraph-server/src/main.py
+++ langgraph-server/src/main.py
@@ -2,18 +2,18 @@ import os
 from langgraph.prebuilt import create_react_agent
 from langchain_openai import ChatOpenAI
 from pydantic import SecretStr
+from langgraph.graph.state import CompiledStateGraph
+
+from custom_react import ChatState, create_custom_react_agent, create_simple_chat_agent
 
 
-def configure_phoenix():
+def _configure_phoenix():
     from phoenix.otel import register
 
     # configure the Phoenix tracer
-    tracer_provider = register(
+    register(
         auto_instrument=True,  # Auto-instrument your app based on installed OI dependencies
     )
-
-
-configure_phoenix()
 
 
 def create_model():
@@ -30,28 +30,62 @@ def create_model():
     api_key = SecretStr(api_key)
 
     model = ChatOpenAI(
-        base_url="https://openrouter.ai/api/v1", model=model_name, api_key=api_key
+        base_url="https://openrouter.ai/api/v1",
+        model=model_name,
+        api_key=api_key,
+        temperature=0.0,
     )
 
     return model
 
 
-def search(query: str) -> str:
-    """Call to surf the web."""
-    if "sf" in query.lower() or "san francisco" in query.lower():
+def get_weather(location: str) -> str:
+    """Gets the weather for a given location."""
+    if "sf" in location.lower() or "san francisco" in location.lower():
         return "It's 60 degrees and foggy."
     return "It's 90 degrees and sunny."
 
 
-agent_graph = create_react_agent(create_model(), tools=[search])
+def tallest_building_faq(location: str) -> str:
+    """Gets info on the tallest building in a given location, including its height."""
+    return "The tallest building in Seattle is the Columbia Center, which is 967 feet tall."
 
-print(agent_graph.get_graph().draw_mermaid())
 
-# inputs = {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+def _run_chat_loop(graph: CompiledStateGraph):
+    conversation_history: ChatState = {"messages": []}
 
-# for s in agent_graph.stream(inputs, stream_mode="values"):
-#     message = s["messages"][-1]
-#     if isinstance(message, tuple):
-#         print(message)
-#     else:
-#         message.pretty_print()
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        user_message = {"role": "user", "content": user_input}
+
+        conversation_history["messages"].append(user_message)
+
+        for event in graph.stream(conversation_history):
+            for value in event.values():
+                assistant_message = value["messages"][-1]
+                conversation_history["messages"].append(assistant_message)
+                print("Assistant:", value["messages"][-1])
+
+
+def main():
+    _configure_phoenix()
+
+    # agent_graph = create_react_agent(create_model(), tools=[get_weather])
+    agent_graph = create_custom_react_agent(
+        create_model(), tools=[get_weather, tallest_building_faq]
+    )
+    # agent_graph = create_simple_chat_agent(create_model())
+
+    print(agent_graph.get_graph().draw_mermaid())
+
+    _run_chat_loop(agent_graph)
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
