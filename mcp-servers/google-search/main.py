5cd485f77232140aa16d2cc32d92759a425732f5
andy <andysalerno@gmail.com>
2025-04-30 21:40:29 -0700

    remove old tool file

---
+++ mcp-servers/google-search/main.py
@@ -0,0 +1,32 @@
+from mcp.server.fastmcp import FastMCP
+from langchain_google_community import GoogleSearchAPIWrapper
+import logging
+import json
+
+logger = logging.getLogger(__name__)
+mcp = FastMCP("google-search")
+search_wrapper = GoogleSearchAPIWrapper()
+
+TOP_N = 8
+
+
+@mcp.tool()
+async def search(query: str) -> str:
+    """
+    Search Google for the given query. Returns the search results (including snippet and url) in JSON format.
+    Useful for looking up current events.
+
+    Args:
+        query: The query to search for. Remember, this is a google query, so write your query as you would in Google.
+    """
+    logger.info("Searching for query: %s", query)
+    results = search_wrapper.results(query, num_results=TOP_N)
+    return json.dumps(results)
+
+
+def main():
+    mcp.run(transport="sse")
+
+
+if __name__ == "__main__":
+    main()
