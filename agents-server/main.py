e4bd5efa5093b16a690a225dde9c8ed982c22225
andy <andysalerno@gmail.com>
2025-05-17 17:58:52 -0700

    update smolagents playground

--- agents-server/main.py
+++ agents-server/main.py
@@ -1,16 +1,10 @@
-# add smolagents/src/smolagents dir to the import path:
-import sys
-
-sys.path.append("./smolagents/src")
-
 import os
-from tools import SearchAndScrape
-from smolagents import OpenAIServerModel, CodeAgent, ToolCallingAgent
+from smolagents import OpenAIServerModel, CodeAgent, WebSearchTool, VisitWebpageTool
 
 from phoenix.otel import register
 from openinference.instrumentation.smolagents import SmolagentsInstrumentor
 
-register()
+register(auto_instrument=True)
 SmolagentsInstrumentor().instrument()
 
 OPENROUTER_KEY = os.environ.get("LLM_API_KEY")
@@ -28,33 +22,8 @@ model = OpenAIServerModel(
     api_base="https://openrouter.ai/api/v1",
 )
 
-search_tool = SearchAndScrape.SearchAndScrape(
-    scraper_endpoint="http://localhost:8002/scrape",
-    scores_endpoint="http://localhost:8001/scores",
-    max_sites_to_scrape=7,
-    max_chunks_to_return=4,
-)
 
-
-def create_research_agent():
-    name = "research_agent"
-    description = "An agent that can perform Google searches and find up-to-date information on any given topic."
-
-    research_agent = CodeAgent(
-        tools=[search_tool],
-        model=model,
-        name=name,
-        description=description,
-        planning_interval=1,
-    )
-
-    return research_agent
-
-
-main_agent = CodeAgent(tools=[], managed_agents=[create_research_agent()], model=model)
-main_agent.prompt_templates["system_prompt"] += (
-    "\n\nA note on your persona: You are a friendly agent that provides final answers to the user. Your final answers should be polite, and maybe a bit fun or whimsical. For anything involving research or external information, delegate to the research agent. Remember that the user will only see the text you provide as an argument to `final_answer(...)`."
-)
+main_agent = CodeAgent(tools=[WebSearchTool(), VisitWebpageTool()], model=model)
 
 while True:
     try:
