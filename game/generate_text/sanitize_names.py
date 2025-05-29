515a6237857a8b7c1ef9dc5c87a2673760e70f36
andy <andysalerno@gmail.com>
2025-05-28 21:04:22 -0700

    remove game

--- game/generate_text/sanitize_names.py
+++
@@ -1,9 +0,0 @@
-from pydantic_ai.models import Model
-
-
-def is_person_safe_name(name: str, model: Model) -> bool:
-    """
-    The purpose of the game is to have fun,
-    so let's filter out names that would not be appropriate or not in the right spirit.
-    """
-    return False
